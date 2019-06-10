e91bac758766cacbe6bf5dacdf2addfad2e3379c
Daniel Ehrlich <ehrlichdaniel1@gmail.com>
2019-06-10 19:27:26 +0300

    Update index.js

--- BackTrack/src/index.js
+++ BackTrack/src/index.js
@@ -32,6 +32,7 @@ function mobileMenu() {
     HM.addEventListener('click', () => {
     
         MN.style.transition = '500ms ease-in-out';
+        CC.style.transition = '.7s ease-in-out';
     
         if(clicked === false) {
             MN.style.transform = 'translateX(41vw)'; 
@@ -48,20 +49,19 @@ function mobileMenu() {
 };
 
 function contentTransitionShow(CC) {
-    CC.style.transition = '.7s ease-in-out';
     CC.style.opacity = 1;
 };
 
 function contentTransitionHide(CC) {
-    CC.style.transition = '.7s ease-in-out';
     CC.style.opacity = 0;
+    
     setTimeout(() => {
         hidingDisplay(CC);
     }, 700);
 };
 
-function hidingDisplay(CC) {
-    CC.style.display = 'none';
+function hidingDisplay(content) {
+    content.style.display = 'none';
 };
 
 // function loadData() {
