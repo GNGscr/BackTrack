0fd34da80e2c5b60b3e9f5401ecf20de6b230249
Daniel Ehrlich <ehrlichdaniel1@gmail.com>
2019-06-10 10:35:36 +0300

    Update index.js

--- BackTrack/src/index.js
+++ BackTrack/src/index.js
@@ -1,20 +1,100 @@
-const HM = document.querySelector('.hamburger-menu');
-const MN = document.querySelector('.mobile-nav');
-const CC = document.querySelector('.content-cover');
+// import { inherits } from 'util';
 
-let clicked = false;
 
-HM.addEventListener('click', () => {
+// let templates = {};
+// templates.imgTemplate = `
+// <div class="item">
+//     <img class="box-pic" src={{placeholder}} alt="Beatles img" srcset="">
+// </div>`;
+// templates.imageTemplate = `
+// <div class="image fade">
+// <img class="box-pic" src={{"../IMG/album19.jpeg"}} alt="Beatles img" srcset="">
+// </div>`;
 
-    MN.style.transition = '500ms ease-in-out';
 
-    if(clicked === false) {
-        MN.style.transform = 'translateX(41vw)'; 
-        clicked = true;
-        CC.style.display = 'flex';
-    } else if(clicked === true) {
-        MN.style.transform = 'translateX(-41vw)';
-        clicked = false;
-        CC.style.display = 'none';
-    }
-});
+
+(function init() {
+    mobileMenu();
+    // createItems();
+    // loadData();
+    // templateUser();
+})();
+
+
+function mobileMenu() {
+
+    const HM = document.querySelector('.hamburger-menu');
+    const MN = document.querySelector('.mobile-nav');
+    const CC = document.querySelector('.content-cover');
+
+    let clicked = false;
+    
+    HM.addEventListener('click', () => {
+    
+        MN.style.transition = '500ms ease-in-out';
+    
+        if(clicked === false) {
+            MN.style.transform = 'translateX(41vw)'; 
+            clicked = true;
+            CC.style.display = 'flex';
+        } else if(clicked === true) {
+            MN.style.transform = 'translateX(-41vw)';
+            clicked = false;
+            CC.style.display = 'none';
+        }
+    });
+};
+
+// function loadData() {
+//     fetch("./data/ex_data.json")
+//         .then(res => res.json())
+//         .then((data) => {
+//             templatesUser(data.innerData);
+//         }
+//     );
+// };
+
+
+// function createItems() {
+
+//     let item = document.createElement('div');
+//     // item.className = item;
+//     item.classList.add('item');
+    
+//     let img = document.createElement('img');
+//     img.type = 'img';
+//     img.setAttribute('src', '');
+//     img.setAttribute('alt', '');
+
+//     img.innerHTML = `<img class="box-pic" src={${code}} alt="Beatles img" srcset="">`;
+//     item.appendChild(img);
+    
+//     const items;
+//     items.forEach(item => {
+        
+//         item = `
+//         <div class="item">
+//         <img class="box-pic" src={${item}} alt={${loopItemName}} srcset="">
+//         </div>
+//         `;
+//     });
+//     LNG.appendChild(items);
+// }
+
+
+// function templatesUser(items) {
+
+    // const LNG = document.querySelector('.long');
+
+    // Array.from(something).map(item => {
+    //     item.innerHTML = ``;
+    // });
+    
+//     let items = [];
+//     items.push();
+    
+//     items.forEach(item => {
+//         img.innerHTML += templates.imgTemplate.replace("{{placeholder}}", item.name.id);
+//     });
+//     LNG.appendChild(items);
+// };
